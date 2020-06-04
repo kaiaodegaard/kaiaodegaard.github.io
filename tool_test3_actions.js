a678fd82ee6d86c1fc2d0db576ec0277dada0afb
kaiaodegaard <57128778+kaiaodegaard@users.noreply.github.com>
2020-06-04 12:53:56 +0200

    Add files via upload

---
+++ tool_test3_actions.js
@@ -0,0 +1,121 @@
+var sc_AnSceneNodeScene_201_0_4_fn = function() {
+//
+//var _this = this;
+///*
+//Clicking on the specified symbol instance executes a function.
+//*/
+//_this.try_again1.on('click', function(){
+///*
+//Moves the playhead to the specified frame number in the timeline and stops the movie.
+//Can be used on the main timeline or on movie clip timelines.
+//*/
+//_this.gotoAndStop(84);
+//});
+//
+//
+};
+var sc_AnSceneNodeScene_201_84_0_fn = function() {
+//
+//var _this = this;
+///*
+//Stop a Movie Clip/Video
+//Stops the specified movie clip or video.
+//*/
+//_this.stop();
+//
+//
+//var _this = this;
+///*
+//Clicking on the specified symbol instance executes a function.
+//*/
+//_this.no_button1.on('click', function(){
+///*
+//Moves the playhead to the specified frame number in the timeline and stops the movie.
+//Can be used on the main timeline or on movie clip timelines.
+//*/
+//_this.gotoAndStop(87);
+//});
+//
+//
+};
+var sc_AnSceneNodeScene_201_84_1_fn = function() {
+//
+//var _this = this;
+///*
+//Clicking on the specified symbol instance executes a function.
+//*/
+//_this.yes_button1.on('click', function(){
+///*
+//Moves the playhead to the specified frame number in the timeline and stops the movie.
+//Can be used on the main timeline or on movie clip timelines.
+//*/
+//_this.gotoAndStop(85);
+//});
+//
+//
+};
+var sc_AnSceneNodeScene_201_85_0_fn = function() {
+//
+//var _this = this;
+///*
+//Clicking on the specified symbol instance executes a function.
+//*/
+//_this.no_button2.on('click', function(){
+///*
+//Moves the playhead to the specified frame number in the timeline and stops the movie.
+//Can be used on the main timeline or on movie clip timelines.
+//*/
+//_this.gotoAndStop(86);
+//});
+//
+//
+};
+var sc_AnSceneNodeScene_201_85_1_fn = function() {
+//
+//var _this = this;
+///*
+//Clicking on the specified symbol instance executes a function.
+//*/
+//_this.yes_button2.on('click', function(){
+///*
+//Moves the playhead to the specified frame number in the timeline and stops the movie.
+//Can be used on the main timeline or on movie clip timelines.
+//*/
+//_this.gotoAndStop(88);
+//});
+//
+//
+};
+var sc_AnSceneNodeScene_201_86_0_fn = function() {
+//
+//var _this = this;
+///*
+//Clicking on the specified symbol instance executes a function.
+//*/
+//_this.no_button3.on('click', function(){
+///*
+//Moves the playhead to the specified frame number in the timeline and stops the movie.
+//Can be used on the main timeline or on movie clip timelines.
+//*/
+//_this.gotoAndStop(89);
+//});
+//
+//
+};
+var sc_AnSceneNodeScene_201_86_1_fn = function() {
+//
+//var _this = this;
+///*
+//Clicking on the specified symbol instance executes a function.
+//*/
+//_this.yes_button3.on('click', function(){
+///*
+//Moves the playhead to the specified frame number in the timeline and stops the movie.
+//Can be used on the main timeline or on movie clip timelines.
+//*/
+//_this.gotoAndStop(88);
+//});
+//
+//
+};
+
